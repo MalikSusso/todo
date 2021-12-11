f2b5e4aec0b6b6cb54dbb2351c224c64c58af319
MalikSusso <malik.gfx@gmail.com>
2021-12-11 19:17:51 +0100

    v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ParagraphComponent } from './paragraph/paragraph.component';
+import { TitleComponent } from './title/title.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ParagraphComponent,
+    TitleComponent
   ],
   imports: [
     BrowserModule
